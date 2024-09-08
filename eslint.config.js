4d5c612d01ed87ae6d3662abd8b85aa1604817da
Пухальский Алексей Юрьевич <APukhalskiy@alfabank.ru>
2024-09-08 22:04:00 +0300

    init: set up vite project

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import EslintImport from 'eslint-plugin-import'
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'import': EslintImport,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      "import/order": 
+      [
+        'error', 
+        { "groups": 
+            [
+              "external", 
+              "builtin", 
+              "internal", 
+              "sibling", 
+              "parent", 
+              "index"
+            ], 
+        } 
+      ],
+    },
+  },
+)
